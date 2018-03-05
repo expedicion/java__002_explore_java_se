6b5359b505ba17fc9bfd91a894ce0fdab5bfd29b
Nostromo <mks.nostromo@gmail.com>
2018-03-05 22:53:09 +0100

    Section 1 - exercises have been done.

--- section 01/src/example_01/PrintSth.java
+++ section 01/src/example_01/PrintSth.java
@@ -3,5 +3,9 @@ package example_01;
 class PrintSth {
     public static void main(String args[]) {
         System.out.println("... 1 2 3 | Boom!");
+
+        System.out.print("... 1 2 3 | Boom!\n");
+
+        System.out.printf("... 1 2 3 | %s", "Boom!");
     }
 }
